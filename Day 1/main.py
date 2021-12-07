c73799eb4b9f5aa685705367223c5c2ba18c5308
Vinicius Garcia <viniciusgarcia1300@gmail.com>
2021-12-06 22:13:51 -0300

    Push

--- Day 1/main.py
+++ Day 1/main.py
@@ -2,8 +2,3 @@ print("Welcome to the Band Name Generator.")
 city = input("What's name of the city you grew up in? ")
 pet = input(" What's your pet's name? ")
 print("Your Band Name could be "+city +" "+ pet)
-
-
-
-
-
